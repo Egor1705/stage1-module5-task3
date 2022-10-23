f9242e4dcd0239f867ed6de71a2cc08863bc3a88
kachanau <BsuHelp@tut.by>
2022-10-23 14:50:15 +0200

    streams

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,43 @@
 package com.epam.mjc;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().filter(n->n%2==0).collect(Collectors.toList());
+      //  throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().map(s->s.toUpperCase()).collect(Collectors.toList());
+       // throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Optional<Integer> var =  list.stream().max(Comparator.naturalOrder());
+        if(var.isPresent()){
+            var.get();
+        }
+        return var;
+       // throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<Integer> newList = list.stream().flatMap(l->l.stream()).collect(Collectors.toList());
+        Optional<Integer> var = newList.stream().min(Comparator.naturalOrder());
+        if(var.isPresent()){
+            var.get();
+        }
+        return var;
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list.stream().reduce(0,(a,b)->a+b);
+        //throw new UnsupportedOperationException("You should implement this method.");
     }
 }
